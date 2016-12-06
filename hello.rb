f27fcd24652fd0a2bec869d04eb582a172847570
keon Joe <keon.ye@lcola.com>
2016-12-06 21:24:33 +0800

    #1855 国家法定垃圾股发达了噶

---
+++ hello.rb
@@ -0,0 +1,18 @@
+###############################################################################
+#                                                                             #
+# Author: Staven Vanderbilt - staven.vanderbilt@gmail.com                     #
+#                                                                             #
+# QQ : 923223553                                                              #
+#                                                                             #
+# Last modified: 2016-12-06 21:19
+#                                                                             #
+# Filename: hello.rb
+#                                                                             #
+# Description:                                                                #
+#                                                                             #
+###############################################################################
+#!/usr/bin/env ruby
+# -*- coding:UTF-8 -*-
+
+
+puts "Hello world"
